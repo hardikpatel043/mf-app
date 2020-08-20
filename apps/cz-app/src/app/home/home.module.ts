c6c1d34737fea1647b359cba68e5f935a3349589
Hardik Arvind Kumar Patel <HPatel5@slb.com>
2020-08-20 16:14:10 +0530

    updated

--- apps/cz-app/src/app/home/home.module.ts
+++ apps/cz-app/src/app/home/home.module.ts
@@ -2,11 +2,10 @@ import { CardModule } from '@mf-app/card';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { MaterialModule } from '../material.module';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MaterialModule, CardModule],
+  imports: [CommonModule, HomeRoutingModule, CardModule],
 })
 export class HomeModule {}
