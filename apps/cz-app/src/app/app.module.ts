e84e6b62844321654b6f83f47940588089b80f72
Hardik Arvind Kumar Patel <HPatel5@slb.com>
2020-08-28 15:49:39 +0530

    home app created

--- apps/cz-app/src/app/app.module.ts
+++ apps/cz-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ContentComponent } from './components/content/content.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material.module';
 import { NgModule } from '@angular/core';
@@ -15,7 +14,6 @@ import { NgModule } from '@angular/core';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
