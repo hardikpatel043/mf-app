74a72a2e97d4bb1b65df55f4b3af1fc7eec48f06
Hardik Arvind Kumar Patel <HPatel5@slb.com>
2020-08-28 15:53:47 +0530

    aboutus app created

---
+++ apps/aboutus/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { CardModule } from '@mf-app/card';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    CardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
