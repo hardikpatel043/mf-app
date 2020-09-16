83b6efef5d7f2d0eec696db62c893c11db7699e7
Hardik Arvind Kumar Patel <HPatel5@slb.com>
2020-09-16 14:02:08 +0530

    contact us has runtime loading of element

--- apps/cz-app/src/app/contactus/contactus.component.ts
+++ apps/cz-app/src/app/contactus/contactus.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mf-app-contactus',
   templateUrl: './contactus.component.html',
-  styleUrls: ['./contactus.component.css']
+  styleUrls: ['./contactus.component.css'],
 })
 export class ContactusComponent implements OnInit {
+  selectedItem = 'Aboutus';
+  controls: any[] = [{ name: 'Home' }, { name: 'Aboutus' }];
+  selectedConfig = {
+    tag: 'mf-aboutus',
+    url: 'aboutus/main-es2015.js',
+    isModule: false,
+    content: 'Increment',
+    actionName: 'increment',
+  };
+  dynamicConfigs = [
+    {
+      tag: 'mf-aboutus',
+      url: 'aboutus/main-es2015.js',
+      isModule: false,
+      content: 'Increment',
+      actionName: 'increment',
+    },
+    {
+      tag: 'mf-home',
+      url: 'home/main-es2015.js',
+      isModule: false,
+      content: 'Increment',
+      actionName: 'increment',
+    },
+  ];
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectionChange($event) {
+    if (this.selectedItem === 'Aboutus') {
+      this.selectedConfig = Object.assign(this.dynamicConfigs[0]);
+    } else {
+      this.selectedConfig = Object.assign(this.dynamicConfigs[1]);
+    }
+    console.log(this.selectedItem);
+    console.log(this.selectedConfig);
   }
-
 }
--- apps/cz-app/src/app/contactus/contactus.module.ts
+++ apps/cz-app/src/app/contactus/contactus.module.ts
@@ -1,10 +1,21 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { ContactusComponent } from './contactus.component';
 import { ContactusRoutingModule } from './contactus-routing.module';
-import { NgModule } from '@angular/core';
+import { LazyElementsModule } from '@angular-extensions/elements';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [ContactusComponent],
-  imports: [CommonModule, ContactusRoutingModule],
+  imports: [
+    CommonModule,
+    ContactusRoutingModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    LazyElementsModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ContactusModule {}
