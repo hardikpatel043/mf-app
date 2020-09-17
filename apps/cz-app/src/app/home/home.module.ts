4b518b2b24b126d358016a19a82667da5a211ff2
Hardik Arvind Kumar Patel <HPatel5@slb.com>
2020-09-17 14:33:51 +0530

    externalized material

--- apps/cz-app/src/app/home/home.module.ts
+++ apps/cz-app/src/app/home/home.module.ts
@@ -1,13 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { LazyElementsModule } from '@angular-extensions/elements';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { LazyElementsModule } from '@angular-extensions/elements';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, LazyElementsModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    LazyElementsModule,
+    MatButtonModule,
+  ],
   exports: [HomeComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
